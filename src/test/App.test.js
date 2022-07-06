38af1ab57fda9e6bb31967e3ccf20927d12d7c63
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-06 22:33:44 +0100

    Added a test but yet to set it running

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import {
+  render,
+  //  fireEvent, waitFor, screen
+} from '@testing-library/react';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/calculator/i);
-  expect(linkElement).toBeInTheDocument();
+// import { render, fireEvent, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from '../App';
+
+describe('Testing the pages of the app individually', () => {
+  test('Checks if the page renders accordingly', () => {
+    const page = render.create(
+      <Router>
+        <App />
+      </Router>,
+    )
+      .toJSON();
+    expect(page).toMatchSnapshot();
+  });
 });
