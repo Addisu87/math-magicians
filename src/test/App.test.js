be48c951b07dd282cde8880b57b101c921f22df0
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-06 23:54:32 +0100

    Added some tests but yet to fix the last test.

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,9 +1,9 @@
 import renderer from 'react-test-renderer';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import App from '../App';
 
-describe('App', () => {
+describe('Body of the whole App', () => {
   it('renders correctly', () => {
     const tree = renderer
       .create(
@@ -13,5 +13,48 @@ describe('App', () => {
       )
       .toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  test('Expect it to render the home page', async () => {
+      render(
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>
+      )
+    expect(screen.getByText(/Math magicians is a website for all fans of mathematics./)).not.toBeNull();
+  });
+
+
+  test('Making sure the quote page renders', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const quote = screen.getByText('Quotes');
+
+    fireEvent.click(quote);
+
+    const speaker = screen.getByText(/Albert Einstein/);
+    expect(speaker).not.toBeNull();
+  });
+
+
+  test('Activating the Calculator function', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const calcLink = screen.getByText('Calculator');
+    fireEvent.click(calcLink);
+    const acBtn = screen.getByText('AC');
+    expect(acBtn).not.toBeNull();
+    const prodBtn = screen.getByText('x');
+    expect(prodBtn).not.toBeNull();
+    const equBtn = screen.getByText('=');
+    expect(equBtn).not.toBeNull();
   });
 });
