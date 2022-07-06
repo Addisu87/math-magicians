57a44e3443579883b18052bf3ff979d8c7354c87
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-07-06 22:17:42 +0300

    Remove button.test.js file

--- src/test/Button.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Button from '../components/Button';
-
-it('renders correctly', () => {
-  const tree = renderer.create(<Button />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
